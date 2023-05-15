981b1336a01d8fc2bc2986836b1f9d5cc8f37e1e
RainBowT <rainbowt0506@gmail.com>
2023-05-15 11:48:33 +0800

    [test] quick test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
+import useBookSearch from "./useBookSearch";
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [pageNum, setPageNum] = useState(1);
+
+  function handleSearch(e) {
+    setQuery(e.target.value);
+    setPageNum(1);
+  }
+
+  useBookSearch(query, pageNum);
+
   return (
     <>
-      <input type="text"></input>
+      <input type="text" onChange={handleSearch}></input>
       <div>Title</div>
       <div>Title</div>
       <div>Title</div>
