19dec719a2d5d8cf8879e505d205bd5e260e7706
RainBowT <rainbowt0506@gmail.com>
2023-05-15 14:14:01 +0800

    [feat] implement pagination function

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useState } from "react";
+import { useCallback, useRef, useState } from "react";
 import useBookSearch from "./useBookSearch";
 
 function App() {
   const [query, setQuery] = useState("");
   const [pageNum, setPageNum] = useState(1);
 
+  const { books, hasMore, loading, error } = useBookSearch(query, pageNum);
+
+  const observer = useRef();
+  const lastBookElementRef = useCallback((node) => {
+    if (loading) return;
+    if (observer.current) observer.current.disconnect();
+    observer.current = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting && hasMore) {
+        setPageNum((prevPageNum) => prevPageNum + 1);
+      }
+    });
+    if (node) observer.current.observe(node);
+    console.log(node);
+  });
+
   function handleSearch(e) {
     setQuery(e.target.value);
     setPageNum(1);
   }
 
-  const { books, hasMore, loading, error } = useBookSearch(query, pageNum);
-
   return (
     <>
-      <input type="text" onChange={handleSearch}></input>
-      {books.map((book) => {
-        return <div key={book}>{book}</div>;
+      <input type="text" value={query} onChange={handleSearch}></input>
+      {books.map((book, index) => {
+        if (books.length === index + 1) {
+          return (
+            <div ref={lastBookElementRef} key={book}>
+              {book}
+            </div>
+          );
+        } else {
+          return <div key={book}>{book}</div>;
+        }
       })}
       <div>{loading && "Loading..."}</div>
       <div>{error && "Error"}</div>
